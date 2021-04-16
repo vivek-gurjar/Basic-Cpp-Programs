23efc3e96abbb5c80b0a45e63aab8a32eb98a996
Vivek Gurjar <81826848+vivek-gurjar@users.noreply.github.com>
2021-04-16 06:31:52 -0700

    Create patterns_01.cpp

---
+++ NumberPatterns/patterns_01.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+    int num,i,j;
+
+    cout<<" enter the number --> ";
+    cin>>num;
+    for ( i=1; i<=num; i++)
+    {
+        for ( j=1; j<=num; j++)
+        {
+            cout<<" * ";
+        }
+    cout<<endl;
+    }
+    
+
+}

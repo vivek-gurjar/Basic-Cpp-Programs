2c5114abaac8563e12a2586c420a9254092b3275
Vivek Gurjar <81826848+vivek-gurjar@users.noreply.github.com>
2021-04-17 03:53:05 -0700

    Create patterns_01.cpp

---
+++ SymbolPatterns/patterns_01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,num;
+    cout<<" Enter the Numer --> ";
+    cin>>num;
+    for(i=1; i<=num; i++)
+    {
+        for(j=1; j<=num; j++)
+        {
+            cout<<"  "<<"*";
+        }
+        cout<<endl;
+        
+    }
+}
